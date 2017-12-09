cc20a60f51e71eb51be60fcd8c776a56abdce185
huilinqiu <187346709@qq.com>
2017-12-09 10:54:11 +0800

    second

---
+++ js/common.js
@@ -0,0 +1,30 @@
+//复制本页链接到clipboard中
+	var copykey = document.querySelector('.is-link.m-copy');
+	copykey.addEventListener('click',function(event){
+		clipboard.copy(location.href);
+	});
+	
+	//分享
+	var sharekey = document.querySelector('.panel-heading a.is-success');
+	var dropmenu = document.querySelector('.m-dropdown-menu');
+	var display = getComputedStyle(dropmenu,null).getPropertyValue('display');
+	dropmenu.addEventListener('mouseleave',function(){
+		dropmenu.style.display = 'none';
+	});
+	dropmenu.addEventListener('mouseenter',function(){
+		if(display === 'none'){
+			dropmenu.style.display = 'block';
+		}else{
+			dropmenu.style.display = 'none';
+		}
+	});
+	sharekey.addEventListener('mouseleave',function(){
+		dropmenu.style.display = 'none';
+	});
+	sharekey.addEventListener('mouseenter',function(){
+		if(display === 'none'){
+			dropmenu.style.display = 'block';
+		}else{
+			dropmenu.style.display = 'none';
+		}
+	});
